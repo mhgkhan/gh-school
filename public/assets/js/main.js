ae706dc21e97f3c490a2327666b3e1fab724dbd4
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2023-09-23 22:47:42 +0500

    added testemonial and contactus section

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -15,41 +15,29 @@ menuBtn.addEventListener("click", e => {
     }
 })
 
-closeMenu.addEventListener("click", e=>{
+closeMenu.addEventListener("click", e => {
     navMenu.style.transform = "translateY(-500px)"
     open = true
 })
 
-let surveyValue = document.querySelectorAll(".surveyValue");
-let counter1=0,counter2=0,counter3 = 0
-setInterval(() => {
-    if(counter1 ===5000){
-        clearInterval();
-    }
-    else{
-        counter1 = counter1 + 500
-        surveyValue[0].innerHTML = counter1+"+"
-    }
-}, 100);
-setInterval(() => {
-    if(counter2 ===50000){
-        clearInterval();
-    }
-    else{
-        counter2 = counter2 + 200
-        surveyValue[1].innerHTML = counter2+"+"
-    }
-}, 100);
 
-setInterval(() => {
-    if(counter3 ===100000){
-        clearInterval();
-    }
-    else{
-        counter3 = counter2 + 100
-        surveyValue[2].innerHTML = counter3+"+"
-    }
-}, 100);
+const increaseValues = (range, index,timing) => {
+    let counter = 0
+    let surveyValue = document.querySelectorAll(".surveyValue");
+    setInterval(() => {
+        if (counter >= range) {
+            clearInterval();
+        }
+        else {
+            counter = counter  + 353
+            surveyValue[index].innerHTML = counter + "+"
+        }
+    }, timing);
+}
+
+increaseValues(6000, 0,150);
+increaseValues(16000, 1,100);
+increaseValues(4500, 2, 200)
 
 
 
@@ -64,12 +52,12 @@ var typed = new Typed("#firstHead", {
 var typed = new Typed("#courses", {
     strings: ["MATHS", "URDU", "ENGLISH", "ISLAMYAT", "PAK STUDY", "PASHTO", "BIOLOGY", "CHEMISTARY"],
     typeSpeed: 50,
-    loop:true,
-    cursorChar:"✏️"
+    loop: true,
+    cursorChar: "✏️"
 })
 
 AOS.init({
-    duration:600,
-    once:false,
-    offset:30
+    duration: 600,
+    once: false,
+    offset: 30
 })
