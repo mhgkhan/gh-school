12fbf09935bbebca0ddb71d7c49582b610d90e34
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2023-09-23 13:06:42 +0500

    afasdfas

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -20,6 +20,36 @@ closeMenu.addEventListener("click", e=>{
     open = true
 })
 
+let surveyValue = document.querySelectorAll(".surveyValue");
+let counter1=0,counter2=0,counter3 = 0
+setInterval(() => {
+    if(counter1 ===5000){
+        clearInterval();
+    }
+    else{
+        counter1 = counter1 + 500
+        surveyValue[0].innerHTML = counter1+"+"
+    }
+}, 100);
+setInterval(() => {
+    if(counter2 ===50000){
+        clearInterval();
+    }
+    else{
+        counter2 = counter2 + 200
+        surveyValue[1].innerHTML = counter2+"+"
+    }
+}, 100);
+
+setInterval(() => {
+    if(counter3 ===100000){
+        clearInterval();
+    }
+    else{
+        counter3 = counter2 + 100
+        surveyValue[2].innerHTML = counter3+"+"
+    }
+}, 100);
 
 
 
