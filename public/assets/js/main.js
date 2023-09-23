6b8726dd99bf0e230978a864be1732d8e3a73969
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2023-09-23 07:57:33 +0500

    services cards design completed

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -7,19 +7,16 @@ let open = true
 menuBtn.addEventListener("click", e => {
     if (!open) {
         navMenu.style.transform = "translateY(-500px)"
-        navMenu.style.zIndex = "-1"
         open = true
     }
     else {
         navMenu.style.transform = "translateY(0px)"
-        navMenu.style.zIndex = "1"
         open = false
     }
 })
 
 closeMenu.addEventListener("click", e=>{
     navMenu.style.transform = "translateY(-500px)"
-    navMenu.style.zIndex = "-1"
     open = true
 })
 
